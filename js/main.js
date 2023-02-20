e24403f80aedea08d9843d5bd78f8930c3de31d8
Phillip Sanchez <plang.psm@gmail.com>
2023-02-20 14:19:54 -0800

    client side code

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-alert('working')
+document.querySelector('button').addEventListener('click', apiRequest)
+
+async function apiRequest() {
+    const rapperName = document.querySelector('input').value
+    try {
+        const res = await fetch(`https://plang-psm.github.io/rap-names-class-fun/api/${rapperName}`)
+        const data = await res.json()
+
+        console.log(data)
+        document.querySelector('h2').innerText = data.birthName
+    } catch (err) {
+        console.log(err)
+    }
+}
