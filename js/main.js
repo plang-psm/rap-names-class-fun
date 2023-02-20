b035f0f5e4709920bf6e765e360de88f00058de9
Phillip Sanchez <plang.psm@gmail.com>
2023-02-20 14:40:50 -0800

    url api

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ document.querySelector('button').addEventListener('click', apiRequest)
 async function apiRequest() {
     const rapperName = document.querySelector('input').value
     try {
-        const res = await fetch(`https://plang-psm.github.io/rap-names-class-fun/api/${rapperName}`)
+        const res = await fetch(`https://rap-names-api.netlify.app/${rapperName}`)
         const data = await res.json()
 
         console.log(data)
