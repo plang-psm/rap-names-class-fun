ba381f05238446227dc3e5a4aa18759385ed9544
Phillip Sanchez <plang.psm@gmail.com>
2023-02-21 07:52:41 -0800

    update link to cyclic

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ document.querySelector('button').addEventListener('click', apiRequest)
 async function apiRequest(){
     const rapperName = document.querySelector('input').value
     try{
-        const response = await fetch(`https://rap-names-api.netlify.app/api/${rapperName}`)
+        const response = await fetch(`https://rap-names-class-fun.cyclic.app/api/${rapperName}`)
         const data = await response.json()
 
         console.log(data)
