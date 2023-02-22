6991f4b5b4d91237aeecd8824ac7c7054313488c
Phillip Sanchez <plang.psm@gmail.com>
2023-02-21 17:46:30 -0800

    updated url

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ document.querySelector('button').addEventListener('click', apiRequest)
 async function apiRequest(){
     const rapperName = document.querySelector('input').value
     try{
-        const res = await fetch(`http://localhost:8000/api/${rapperName}`)
+        const res = await fetch(`https://rap-names-class-fun-uh4e.onrender.com/api/${rapperName}`)
         const data = await res.json()
 
         // console.log(data)
