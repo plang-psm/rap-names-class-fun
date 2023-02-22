bb88675cdf772d24cac8b6ed58020b0a76f999a1
Phillip Sanchez <plang.psm@gmail.com>
2023-02-21 18:00:14 -0800

    clean up

--- js/main.js
+++ js/main.js
@@ -1,17 +1,3 @@
-// document.querySelector('button').addEventListener('click', apiRequest)
-
-// async function apiRequest(){
-//     const rapperName = document.querySelector('input').value
-//     try{
-//         const res = await fetch(`http://localhost:8000/api/${rapperName}`)
-//         const data = await res.json()
-
-//         // console.log(data)
-//         document.querySelector('h2').innerText = data.birthName
-//     }catch(error){
-//         console.log(error)
-//     }
-// }
 document.querySelector('button').addEventListener('click', apiRequest)
 
 async function apiRequest(){
@@ -20,7 +6,6 @@ async function apiRequest(){
         const response = await fetch(`https://rap-names-class-fun-uh4e.onrender.com/api/${rapperName}`)
         const data = await response.json()
 
-        console.log(data)
         document.querySelector('h2').innerText = data.birthName
     }catch(error){
         console.log(error)
